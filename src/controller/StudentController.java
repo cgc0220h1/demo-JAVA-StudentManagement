e83e31bb5a4b9eb6f92cb87e80185f7fe92fd33c
vanduc2514 <vanduc2514@gmail.com>
2020-04-02 17:27:15 +0700

    Bổ sung lớp Student Management

--- src/controller/StudentController.java
+++ src/controller/StudentController.java
@@ -1,9 +1,10 @@
 package controller;
 
-import model.Person;
 import model.Student;
 
 public class StudentController extends PersonController {
+
+    public static final double MINIMUM_SCHOLARSHIP_SCORE = 8.0;
 
     public void inputData(Student student) {
         super.inputData(student);
@@ -21,5 +22,13 @@ public class StudentController extends PersonController {
         System.out.printf("StudentID: %s \n",student.getStudentID());
         System.out.printf("Average Score: %.2f \n",student.getAverageScore());
         System.out.printf("Email: %s \n", student.getEmail());
+        System.out.printf("Is Valid for scholarship: %s \n",isValidForScholarship(student));
+    }
+
+    public boolean isValidForScholarship(Student student) {
+        if (student.getAverageScore() >= MINIMUM_SCHOLARSHIP_SCORE) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/model/StudentManagement.java
@@ -0,0 +1,5 @@
+package model;
+
+public abstract class StudentManagement {
+    public abstract void display();
+}
